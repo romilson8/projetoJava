bb43714fff0bf46954b493df3d99855f18dd3591
romilson <zero.barros@gmail.com>
2018-08-29 22:06:31 -0300

    criacao da classe Avaliador

---
+++ src/Avaliador.java
@@ -0,0 +1,39 @@
+public class Avaliador {
+
+    private String nome;
+    private String cref;
+    private String email;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCref() {
+        return cref;
+    }
+
+    public void setCref(String cref) {
+        this.cref = cref;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,7 +1,76 @@
+import java.util.Date;
+
 public class Usuario {
     private String nome;
     private String email;
     private String senha;
+    private String endereco;
+    private Double peso;
+    private Date nascimerto;
+    private Double altura;
+    private Boolean sexo;
+    private String etnia;
+    private String telefone;
+
+    public Usuario(){
+
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Boolean getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEtnia() {
+        return etnia;
+    }
+
+    public void setEtnia(String etnia) {
+        this.etnia = etnia;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Date getNascimerto() {
+        return nascimerto;
+    }
+
+    public void setNascimerto(Date nascimerto) {
+        this.nascimerto = nascimerto;
+    }
 
     public String getNome() {
         return nome;
